4e28200eaf815c0197ccda3d3e412709cef2ca7f
Badri Aran <badriaran10@gmail.com>
2022-12-12 19:33:36 +0545

    First C# introduction

---
+++ HelloApp/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            string name;
+            name= Console.ReadLine("Enter Your Name: ");
+            Console.WriteLine(name);
+        }
+    }
+}
