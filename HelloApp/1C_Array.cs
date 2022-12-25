c181f62cf0021acf32853e1139c981e0cae04196
Badri Aran <badriaran10@gmail.com>
2022-12-25 19:05:50 +0545

    array In c#

---
+++ HelloApp/1C_Array.cs
@@ -0,0 +1,14 @@
+class ArrayLearner
+{
+    void LearnArrays()
+    {
+        //One-Dimension Array:
+        byte[] ages={56,34,12,32,12,10};
+        
+        var numbers=new int[100];
+
+        //Multi-dimension Array:
+        double [,] Matrix=new double[3,4];
+
+    }
+}
