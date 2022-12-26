a4d45770a52cc19a5af51f6c455565ef4f6493e0
Badri Aran <badriaran10@gmail.com>
2022-12-26 13:54:39 +0545

    Array Assigment Completed

--- HelloApp/1C_Array.cs
+++ HelloApp/1C_Array.cs
@@ -2,10 +2,21 @@ class ArrayLearner
 {
     public void ArraysAssigment()
     {
-        string[] names = {"Badri","Ram","Giri","Aran","Shah","Prakesh"};
+        string[] names = {"Badri","Ram","Giri","Aran","Shah","Prakesh","Giri","Aran","Shah","Prakesh"};
         for (int i = 0; i < names.Length; i++)
         {
-            Console.WriteLine(names[i]);
+            Console.WriteLine($"{names[i]}\n");
+        }
+        var salaries= new double[6];
+        Console.WriteLine("Enter Salary:\n");
+        for (int s = 0; s < salaries.Length; s++)
+        {
+            salaries[s]=double.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("The recorded list of Salary are:\n");
+        foreach (var salary in salaries)
+        {
+            Console.WriteLine(salary);
         }
     }
 }
